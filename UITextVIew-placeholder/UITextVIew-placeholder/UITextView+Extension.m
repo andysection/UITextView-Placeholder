87c2a50d5b4f712f7833f052e9131246ec0012f9
andysection <398148947@qq.com>
2016-12-27 16:32:56 +0800

    补

--- UITextVIew-placeholder/UITextVIew-placeholder/UITextView+Extension.m
+++ UITextVIew-placeholder/UITextVIew-placeholder/UITextView+Extension.m
@@ -7,7 +7,51 @@
 //
 
 #import "UITextView+Extension.h"
+#import <objc/runtime.h>
 
 @implementation UITextView (Extension)
+
++ (void)load {
+    
+    // 获取类方法 class_getClassMethod
+    // 获取对象方法 class_getInstanceMethod
+    
+    Method setFontMethod = class_getInstanceMethod(self, @selector(setFont:));
+    Method was_setFontMethod = class_getInstanceMethod(self, @selector(was_setFont:));
+    
+    // 交换方法的实现
+    method_exchangeImplementations(setFontMethod, was_setFontMethod);
+}
+
+- (void)setPlaceholderWithText:(NSString *)text Color:(UIColor *)color{
+    //防止重复设置 cell复用等问题
+    for (UIView *view in self.subviews) {
+        if ([view isKindOfClass:[UILabel class]]) {
+            [view removeFromSuperview];
+        }
+    }
+
+    //设置占位label
+    UILabel *label = [[UILabel alloc] init];
+    label.text = text;
+    label.font = self.font;
+    label.textColor = color;
+    
+    [self addSubview:label];
+    [self setValue:label forKey:@"_placeholderLabel"];
+    
+    //第一次会有位移
+    self.text = @"1";
+    self.text = @"";
+}
+
+- (void)was_setFont:(UIFont *)font{
+    //调用原方法 setFont:
+    [self was_setFont:font];
+    //设置占位字符串的font
+    UILabel *label = [self valueForKey:@"_placeholderLabel"];
+    label.font = font;
+    NSLog(@"%s", __func__);
+}
 
 @end
--- UITextVIew-placeholder/UITextVIew-placeholder/ViewController.m
+++ UITextVIew-placeholder/UITextVIew-placeholder/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <objc/message.h>
+#import "UITextView+Extension.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) UILabel *label;
+@property (nonatomic, strong) UITextView *textView;
+@property (weak, nonatomic) IBOutlet UITextView *xibTextView;
 
 @end
 
@@ -16,14 +22,69 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 通过运行时，发现UITextView有一个叫做“_placeHolderLabel”的私有变量
+    unsigned int count = 0;
+    Ivar *ivars = class_copyIvarList([UITextView class], &count);
+    for (int i = 0; i < count; i++)
+    { Ivar ivar = ivars[i];
+        const char *name = ivar_getName(ivar);
+        NSString *objcName = [NSString stringWithUTF8String:name];
+        NSLog(@"%d : %@",i,objcName);
+    }
+//    [self setupUI];
+    [self setupUIForExtension];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupUIForExtension{
+    UITextView *textView = [[UITextView alloc] init];
+    textView.frame = CGRectMake(5, 100, [UIScreen mainScreen].bounds.size.width -10, 200);
+    [textView setBackgroundColor:[UIColor greenColor]];
+    [self.view addSubview:textView];
+    NSLog(@"%@",textView.font);
+    [textView setPlaceholderWithText:@"占位字符串" Color:[UIColor lightGrayColor]];
+    NSLog(@"%@",textView.font);
+    [textView setPlaceholderWithText:@"占位字符串" Color:[UIColor lightGrayColor]];
+    NSLog(@"%@",textView.font);
+    
+    textView.font = [UIFont systemFontOfSize:30];
 }
+
+- (void)setupUI{
+    UITextView *textView = [[UITextView alloc] init];
+    textView.frame = CGRectMake(5, 100, [UIScreen mainScreen].bounds.size.width -10, 200);
+    [textView setBackgroundColor:[UIColor greenColor]];
+    [self.view addSubview:textView];
+    _textView = textView;
+    
+    UILabel *placeHolderLabel = [[UILabel alloc] init];
+    placeHolderLabel.text = @"new";
+    placeHolderLabel.textColor = [UIColor lightGrayColor];
+    self.xibTextView.backgroundColor = [UIColor orangeColor];
+    [self.xibTextView addSubview:placeHolderLabel];
+    [self.xibTextView setValue:placeHolderLabel forKey:@"_placeholderLabel"];
+    
+    // _placeholderLabel
+    UILabel *placeHolderLabel1 = [[UILabel alloc] init];
+    placeHolderLabel1.text = @"new";
+    placeHolderLabel1.textColor = [UIColor lightGrayColor];
+    [_textView addSubview:placeHolderLabel1];
+    [_textView setValue:placeHolderLabel1 forKey:@"_placeholderLabel"];
+    
+    _textView.text = @"1";
+    _textView.text = @"";
+
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    
+
+}
+- (IBAction)buttonClick:(id)sender {
+    
+    
+}
+
 
 
 @end
