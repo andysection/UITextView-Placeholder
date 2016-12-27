bcc8ec5ec776f94d160a99f047edf66a9184b945
andysection <398148947@qq.com>
2016-12-27 17:19:57 +0800

    优化代码

--- UITextVIew-placeholder/UITextVIew-placeholder/ViewController.m
+++ UITextVIew-placeholder/UITextVIew-placeholder/ViewController.m
@@ -32,11 +32,12 @@
         NSString *objcName = [NSString stringWithUTF8String:name];
         NSLog(@"%d : %@",i,objcName);
     }
-//    [self setupUI];
+    
     [self setupUIForExtension];
 }
 
 - (void)setupUIForExtension{
+    //纯代码
     UITextView *textView = [[UITextView alloc] init];
     textView.frame = CGRectMake(5, 100, [UIScreen mainScreen].bounds.size.width -10, 200);
     [textView setBackgroundColor:[UIColor greenColor]];
@@ -47,42 +48,16 @@
     [textView setPlaceholderWithText:@"占位字符串" Color:[UIColor lightGrayColor]];
     NSLog(@"%@",textView.font);
     
-    textView.font = [UIFont systemFontOfSize:30];
-}
-
-- (void)setupUI{
-    UITextView *textView = [[UITextView alloc] init];
-    textView.frame = CGRectMake(5, 100, [UIScreen mainScreen].bounds.size.width -10, 200);
-    [textView setBackgroundColor:[UIColor greenColor]];
-    [self.view addSubview:textView];
-    _textView = textView;
-    
-    UILabel *placeHolderLabel = [[UILabel alloc] init];
-    placeHolderLabel.text = @"new";
-    placeHolderLabel.textColor = [UIColor lightGrayColor];
-    self.xibTextView.backgroundColor = [UIColor orangeColor];
-    [self.xibTextView addSubview:placeHolderLabel];
-    [self.xibTextView setValue:placeHolderLabel forKey:@"_placeholderLabel"];
-    
-    // _placeholderLabel
-    UILabel *placeHolderLabel1 = [[UILabel alloc] init];
-    placeHolderLabel1.text = @"new";
-    placeHolderLabel1.textColor = [UIColor lightGrayColor];
-    [_textView addSubview:placeHolderLabel1];
-    [_textView setValue:placeHolderLabel1 forKey:@"_placeholderLabel"];
-    
-    _textView.text = @"1";
-    _textView.text = @"";
-
-}
-
-- (void)viewWillAppear:(BOOL)animated{
-    
-
-}
-- (IBAction)buttonClick:(id)sender {
+    textView.font = [UIFont systemFontOfSize:18];
     
     
+    //xib加载
+//    UILabel *placeHolderLabel = [[UILabel alloc] init];
+//    placeHolderLabel.text = @"new";
+//    placeHolderLabel.textColor = [UIColor lightGrayColor];
+//    self.xibTextView.backgroundColor = [UIColor orangeColor];
+//    [self.xibTextView addSubview:placeHolderLabel];
+//    [self.xibTextView setValue:placeHolderLabel forKey:@"_placeholderLabel"];
 }
 
 
